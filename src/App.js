bfb2b35b4ce59cee8cb048e402177ac9e960a52a
Mamady Sillah <supurpisces310@msn.com>
2020-09-14 18:31:48 -0400

    imported home, error, about, and singlecocktail into app.js component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
 
 export default function App() {
   return (
