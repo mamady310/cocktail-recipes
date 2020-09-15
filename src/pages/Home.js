3ec0dd59dc81531994b17c2bd2ef5fc836a5b0af
Mamady Sillah <supurpisces310@msn.com>
2020-09-15 16:28:24 -0400

    resolved error that I had

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,29 +5,30 @@ import SearchForm from '../components/SearchForm';
 export const Home = () => {
     //using react hooks
     const [loading, setLoading] = React.useState(false);
-    const [searchTerm, setSearchTerm] = React.useState("rum");
+    const [searchTerm, setSearchTerm] = React.useState('gin');
     const [cocktails, setCocktails] = React.useState([]);
 
     //http get request
     React.useEffect(() => {
-        async function getCocktails() {
-            try {
-                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`);
-                const data = await response.json();
-                const { cocktails } = data;
-                if (cocktails) {
-                const foundCocktails = cocktails.map(item => {
-                    console.log(item);
-                });
-                } else {
-                    setCocktails([]);
-                }
-            } catch (error) {
-               console.log(error); 
-            }
+
+        async function getCocktails (){
+        try {
+            const response = await 
+            fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+            );
+        const data = await response.json();
+        const { drinks } = data;
+        // console.log(drinks);
+        if(drinks) {
+            console.log(drinks);
+        } else {
+            console.log('noooo!!!');
         }
-        getCocktails();
-       
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getCocktails();      
     }, [searchTerm]);
     return (
         <main>
