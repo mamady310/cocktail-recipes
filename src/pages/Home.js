51e6f4d21bef6695d06db508439c4f004cbc2b87
Mamady Sillah <supurpisces310@msn.com>
2020-09-14 22:23:13 -0400

    created an async awai function to make sure promise is settled before moving on. A try catch statement used to handle errors. If else statement put in if user input does not return any results

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,19 @@ import SearchForm from '../components/SearchForm';
 export const Home = () => {
     //using react hooks
     const [loading, setLoading] = React.useState(false);
-    const [searchTerm, setSearchTerm] = React.useState('d');
+    const [searchTerm, setSearchTerm] = React.useState("");
     const [cocktails, setCocktails] = React.useState([]);
     //http get request
     React.useEffect(()=>{
-        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(res => res.json()).then(data => setCocktails(data.drinks));
+        async function getCocktails() {
+            try {
+                const response = await   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+                
+            } catch (error) {
+               console.log(error); 
+            }
+        }
+       
     }, [searchTerm]);
     return (
         <main>
