c472e20d8637f20934fa6e8ffbe8a6a797d32946
Mamady Sillah <supurpisces310@msn.com>
2020-09-14 21:56:44 -0400

    when you change search terms items returned in console.log are updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ import SearchForm from '../components/SearchForm';
 export const Home = () => {
     //using react hooks
     const [loading, setLoading] = React.useState(false);
-    const [searchTerm, setSearchTerm] = React.useState('a');
+    const [searchTerm, setSearchTerm] = React.useState('d');
     const [cocktails, setCocktails] = React.useState([]);
     //http get request
     React.useEffect(()=>{
-        fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita').then(res => res.json()).then(data => setCocktails(data.drinks));
-    }, []);
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`).then(res => res.json()).then(data => setCocktails(data.drinks));
+    }, [searchTerm]);
     return (
         <main>
             <SearchForm setSearchTerm={setSearchTerm}/>
