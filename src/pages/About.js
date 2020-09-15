73aa95474984cfbd1200e41b0d500d8ad98931da
Mamady Sillah <supurpisces310@msn.com>
2020-09-14 20:44:30 -0400

    finished error page and added styling to it

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
-            Hello from about page
-        </div>
+        <section classname='section about-section'>
+        <h1 className='section-title'>
+            About Us
+        </h1>
+        <p>
+            React Cocktails is an award winning cocktail bar specializing in imported spirits. We prouldy serve specially crafted cocktails that will rock your tastebuds. All in a free spirited space devoid of boredom and silly rules. Please browse our popular cocktails.
+        </p>
+        </section> 
     )
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import booze from '../pages/booze.jpg';
+
 
 export const Error = () => {
     return (
-        <div>
-            hello from error page
+       <section className='error-page section'>
+        <div className='error-container'>
+            <h1>Drinks Not Found</h1>
+            <img className='booze'src={booze} alt="no booze"/>
+            <h1>Go</h1>
+            <Link to='/' className='btn btn-primary'>
+                HOME
+            </Link>
+
         </div>
+       </section>
     )
 }
 
