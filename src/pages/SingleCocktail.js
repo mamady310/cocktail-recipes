89234ac3047ab758fb0842ff9b9331e54bbe63a9
Mamady Sillah <supurpisces310@msn.com>
2020-09-15 21:23:16 -0400

    created asyn getCocktail function that makes http request for cocktails based on their id try/catch and if else statements included

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -4,6 +4,27 @@ import { useParams } from 'react-router-dom';
 export const SingleCocktail = () => {
     // console.log(useParams());
     const {id} = useParams();
+    const [loading, setLoading] = React.useState(false);
+    const [cocktail, setCocktail] = React.useState(null);
+
+    React.useEffect(()=> {
+        setLoading(true);   
+
+        async function getCocktail(){
+            try {
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+                const data = await response.json();
+                if(data.drinks) {
+
+                } else {
+                    setCocktail(null);
+                }
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
+        }
+    }, [id])
     return (
         <div>
             Hello from cocktail page id : {id}
