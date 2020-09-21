e6f2f142d9bdc4e94110152138eaa073511aca7a
Mamady Sillah <supurpisces310@msn.com>
2020-09-21 14:52:57 -0400

    used css template and tweaked it

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,9 +6,9 @@ export const About = () => {
         <h1 className='section-title'>
             About Us
         </h1>
-        <p>
+        {/* <p>
             React Cocktails is an award winning cocktail bar specializing in imported spirits. We prouldy serve specially crafted cocktails that will rock your tastebuds. All in a free spirited space devoid of boredom and silly rules. Please browse our popular cocktails.
-        </p>
+        </p> */}
         </section> 
     )
 }
